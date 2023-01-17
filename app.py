96a3c918c7cf29af7d00f6c16e8d872f4b49dc18
Toshiaki Baba <toshiaki@netmark.jp>
2023-01-17 21:33:26 +0900

    upgrade modules to latest

--- app.py
+++ app.py
@@ -35,7 +35,6 @@ def config():
             _config['db']['passwd'] = password
     return _config
 
-
 _db = None
 
 def db():
@@ -166,12 +165,13 @@ def image_url(post):
     return "/image/%s%s" % (post['id'], ext)
 
 # http://flask.pocoo.org/snippets/28/
-from jinja2 import evalcontextfilter, Markup, escape
+from jinja2 import pass_eval_context
+from markupsafe import Markup, escape
 
 _paragraph_re = re.compile(r'(?:\r\n|\r|\n){2,}')
 
 @app.template_filter()
-@evalcontextfilter
+@pass_eval_context
 def nl2br(eval_ctx, value):
     result = u'\n\n'.join(u'<p>%s</p>' % p.replace('\n', '<br>\n') \
         for p in _paragraph_re.split(escape(value)))
